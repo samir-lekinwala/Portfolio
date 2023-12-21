40cb9e5e5bbc4ea7bfd8e142db8152b52335ef0c
Samir Lekinwala <samirlekinwala@gmail.com>
2023-12-21 17:02:20 +1300

    add /portfolio as home route

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
           <AppHeader />
           <Suspense fallback={''}>
             <Routes>
-              <Route path="/" element={<Home />} />
+              <Route path="/portfolio" element={<Home />} />
               <Route path="projects" element={<Projects />} />
               <Route
                 path="projects/single-project"
