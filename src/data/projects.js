9b8127f588efc20c44193e120deb49f334aff397
Samir Lekinwala <samirlekinwala@gmail.com>
2024-08-08 13:21:14 +1200

    Add Template barber project

--- src/data/projects.js
+++ src/data/projects.js
@@ -13,8 +13,29 @@ import rcmndrImg from '../images/project-images/rcmndr.png'
 import todosImg from '../images/project-images/todos.png'
 import salesLedgerImg from '../images/project-images/sales-ledger-small.png'
 import charmWebsiteImg from '../images/project-images/charm-website-screenshot.jpg'
+import templateBarberImg from '../images/project-images/templatebarber-mobile.png'
 
 export const projectsData = [
+  {
+    id: 9,
+    title: 'Template Barbers',
+    category: 'UI/UX Design',
+    description: [
+      `Template is a project I created based on wanting to create a website for barbers which could use this as a template for their own businesses. I wanted to use this to showcase where my skills are currently too. I started off designing the website in Figma and then when I was happy with how it looked, I started working on different components of the website and putting them together. I have currently only somewhat finished the main page albeit with some components I’d like to have in there missing, like a reviews section.`,
+      'Template is fully responsive so please do check the link and see how it plays on different screen sizes. I put in a lot of effort to make the team members flow nicely on different screen sizes.',
+      'This website surprisingly didn’t take me too long to create. This is a product that I am quite happy with. It shows my skills as a developer are indeed getting stronger. I’m sticking to using React with Tailwind CSS for the most part and even making components on my own, e.g the slideshow on mobile screens was made completely from scratch. However, I’m not currently happy with how the slideshow is, I would like for there to be a smooth animation in between the change of team members. Having that would make the website look a bit smoother.',
+      'I am currently still working on the website since the other pages are yet to be completed so the design is not exactly finalised.',
+    ],
+    skills: `*React
+    *TypeScript
+    *JavaScript
+    *HTML
+    *Tailwind CSS
+    *Material Tailwind
+    `,
+    img: templateBarberImg,
+    websiteLink: 'https://templatebarber.netlify.app/',
+  },
   {
     id: 8,
     title: 'Charm Beauty Parlour',
