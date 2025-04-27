edeebdb8636951bc9be3fcfac811bd93b16a2ee6
Samir Lekinwala <samirlekinwala@gmail.com>
2025-04-28 11:59:26 +1200

    Updates

--- static/js/946.37f5db3a.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_tailwindcss_portfolio=self.webpackChunkreact_tailwindcss_portfolio||[]).push([[946],{405:(s,a,c)=>{c.r(a),c.d(a,{default:()=>l});var e=c(6498),t=c(8812),i=c(184);const l=()=>(0,i.jsx)(t.V,{children:(0,i.jsx)("div",{className:"container mx-auto",children:(0,i.jsx)(e.Z,{})})})}}]);
-//# sourceMappingURL=946.37f5db3a.chunk.js.map
