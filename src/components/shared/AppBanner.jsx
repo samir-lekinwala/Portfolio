08bd3ea66dd8f68697c6766b5b76d15729a00a77
Samir Lekinwala <samirlekinwala@gmail.com>
2025-07-19 17:44:44 +1200

    remove CV

--- src/components/shared/AppBanner.jsx
+++ src/components/shared/AppBanner.jsx
@@ -49,7 +49,7 @@ const AppBanner = () => {
           }}
           className="flex justify-center sm:block"
         >
-          <a
+          {/* <a
             download="Samir_Lekinwala_CV.pdf"
             href="files/Samir_Lekinwala_CV.pdf"
             className="font-general-medium flex justify-center items-center w-36 sm:w-48 mt-12 mb-6 sm:mb-0 text-lg border border-indigo-200 dark:border-ternary-dark py-2.5 sm:py-3 shadow-lg rounded-lg bg-indigo-50 focus:ring-1 focus:ring-indigo-900 hover:bg-indigo-500 text-gray-500 hover:text-white duration-500"
@@ -59,7 +59,7 @@ const AppBanner = () => {
             <span className="text-sm sm:text-lg font-general-medium duration-300">
               Download CV
             </span>
-          </a>
+          </a> */}
         </motion.div>
       </div>
       <motion.div
